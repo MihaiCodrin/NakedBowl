bf58bed40f7ba0d76fb74b22eedf2674dabf4c9a
Mihai <141733462+MihaiCodrin@users.noreply.github.com>
2023-08-16 03:20:29 +0300

    Tests/gitignore

---
+++ Naked Salad BowlTests/Naked_Salad_BowlTests.swift
@@ -0,0 +1,36 @@
+//
+//  Naked_Salad_BowlTests.swift
+//  Naked Salad BowlTests
+//
+//  Created by Alton on 8/18/15.
+//  Copyright (c) 2015 Alton Lin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Naked_Salad_BowlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
